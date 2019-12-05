a33785206ebac89e812ef1e8bba2977d05ca6e02
chaoticalex74 <chaoticalex74@gmail.com>
2019-12-05 23:31:56 +0100

    ChatBotController 1.1 View Chatbot 1.0

--- app/Http/Controllers/ChatBot/ChatBotController.php
+++ app/Http/Controllers/ChatBot/ChatBotController.php
@@ -11,18 +11,38 @@ class ChatBotController extends Controller
 {
 
     public function get($input) {
+        $data = "";
+
+        /*
+         * Keywords : list of regex
+         * Results: list of chatbot's answers
+         */
+
         // $keywords = ChatBotModel::keywords();
+        // $results = ChatBotModel::results($keywords);
         $keywords = array();
+        $results = array();
 
         $args = explode(" ", $input);
 
+        $reliance = array();
+
         foreach ($args as $arg) {
             foreach ($keywords as $k) {
-                $reliance[] = compareStrings($arg, $k);
+                // Key is what the regex stands for as result
+                $reliance[$results[$k]] = compareStrings($arg, $k);
             }
         }
 
+        // Try 100% match, then 80%, then 60%, then stop
+        for($i = 1; $i > 0.6; $i-=0.2) {
+            foreach ($reliance as $key => $val) {
+                if ($val >= $i)
+                    $data .= $key;
+            }
+        }
 
+        view("chatbot", $data);
     }
 
     private static function compareStrings($a, $b) {
